48826ef9afe657c35990b6ce4673ab9e9d8a2340
fanzhang2021 <1400970255@qq.com>
2026-03-23 22:18:29 +0800

    修改了一些东西

--- env_config.ps1
+++ env_config.ps1
@@ -1,13 +1,11 @@
 ﻿# env_config.ps1 - 环境变量配置文件
 
 # 设置环境变量
-# $env:OPENAI_API_KEY = "5a62436b582241baafa0f9ef1e1cb795.QTesPEx6CtgZIGX4"
-$env:OPENAI_API_KEY = "sk-a475b26458b94254a9a386b370179de4"
+$env:OPENAI_API_KEY = "5a62436b582241baafa0f9ef1e1cb795.QTesPEx6CtgZIGX4"
 $env:EMAIL_ADDRESS = "fanzhanghnu@gmail.com"
 $env:EMAIL_PASSWORD = "igde nxbq zkow lagk"
 $env:TO_EMAIL = "1400970255@qq.com"
-# $env:OPENAI_BASE_URL = "https://open.bigmodel.cn/api/paas/v4"
-$env:OPENAI_BASE_URL = "https://api.deepseek.com/v1"
+$env:OPENAI_BASE_URL = "https://open.bigmodel.cn/api/paas/v4"
 $env:SMTP_SERVER = "smtp.gmail.com"
 $env:SMTP_PORT = "587"
 $env:DAYS_BACK = "3"
